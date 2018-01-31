df842e8d40ea23eec8f0bd95c049b24686e62dd0
Takahisa Shiratori <shiratori@serverworks.co.jp>
2018-01-31 03:27:58 +0000

    Add automatic installation of squashfs-tools

--- install_singularity.sh
+++ install_singularity.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -v
 exec > >(tee /var/log/user-data.log || logger -t user-data -s 2> /dev/console) 2>&1
-whoami
-pwd
 yum -y update
 yum -y groupinstall "Development Tools"
+yum -y install squashfs-tools
 git clone https://github.com/singularityware/singularity.git
 cd singularity
 ./autogen.sh
